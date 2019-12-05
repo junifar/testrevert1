c0202b8f2debbc2f2072b08a1f47c2f9131878ad
Junifar Hidayat <junifar@gmail.com>
2019-12-05 10:05:11 +0700

    push

--- coba.go
+++ coba.go
@@ -1,13 +1,14 @@
 package testrever1
 
-type(
-	MyStruct struct{
-		Name 		string
-		Address 	string
+type (
+	MyStruct struct {
+		Name    string
+		Address string
 	}
 
 	Districts struct {
 		DistrictID   int64  `db:"district_id"`
 		DistrictName string `db:"district_name"`
+		test         string
 	}
 )
